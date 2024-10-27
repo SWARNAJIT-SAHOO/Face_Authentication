815db7af938066101551aaf158c384db2d0df6ab
Swarnajit Sahoo <123718373+SWARNAJIT-SAHOO@users.noreply.github.com>
2024-10-27 19:07:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbarr from './components/Navbar';
-import CameraBox from './components/CameraBox';
-import DetailsForm from './components/DeatilsForm';
+import logo from './logo.svg';
+import './App.css';
+
+import DetailsForm from './components/DetailsForm';
+import Navbarss from './components/Navbarss';
+import CameraBox from './components/Camera';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home';
+import PingTest from './components/PingTest';
+import { useState } from 'react';
+import Camera from './components/Camera';
+
+
+
+
+
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+  
+    const renderPage = () => {
+      switch (currentPage) {
+        case 'home':
+          return <Home  setCurrentPage={setCurrentPage} />;
+        case 'camera':
+          return <Camera />;
+        default:
+          return <Home />;
+      }
+    };
   return (
     <div className="App">
-      <Navbarr/>
-      <CameraBox/>
+      <Navbarss setCurrentPage={setCurrentPage} />
+      {renderPage()}
+   
     </div>
   );
 }
+
 export default App;
